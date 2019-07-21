f188b7b4c66abc93220b71d5af22f852726f1ba2
sendtodilip <sendtodilip>
2019-07-21 21:18:34 +0530

    Addding ASP.NET Core Identity

--- KudVenkataYoutube/Startup.cs
+++ KudVenkataYoutube/Startup.cs
@@ -6,6 +6,7 @@ using KudVenkataYoutube.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,6 +28,9 @@ namespace KudVenkataYoutube
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
             services.AddMvc().AddXmlSerializerFormatters();
             services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
             //services.AddScoped<IEmployeeRepository, MockEmployeeRepository>();
@@ -60,6 +64,7 @@ namespace KudVenkataYoutube
             app.UseStaticFiles();
             //app.UseFileServer();
             //app.UseMvcWithDefaultRoute();
+            app.UseAuthentication();
             app.UseMvc(routes =>
             {
                 routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
